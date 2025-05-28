28244b5a30d0c4223daa67e49516f18365086681
Björn Petersen <git@bjoernpetersen.net>
2025-05-29 00:04:20 +0200

    Adjust to SDK changes

--- src/telegram_bot/poll.py
+++ src/telegram_bot/poll.py
@@ -1,23 +1,20 @@
 import telegram
 from timhatdiehandandermaus_sdk import TimApi
 
-from telegram_bot.config import Config
 
-
-async def send_movie_poll(*, config: Config, bot: telegram.Bot):
-    api = TimApi(api_url=config.api.base_url)
+async def send_movie_poll(*, api: TimApi, bot: telegram.Bot, chat_id: int) -> None:
     MAX_POLL_OPTIONS = 10
 
     question = "Which movie do you want to watch?"
     default_options = ["Mir egal"]
 
     movie_count = MAX_POLL_OPTIONS - len(default_options)
-    movie_options = api.queued_movies(limit=movie_count)
+    movie_options = await api.queued_movies(limit=movie_count)
     options = [movie.metadata.title for movie in movie_options]
     options.extend(default_options)
 
-    return await bot.send_poll(
-        chat_id=config.telegram.poll_chat_id,
+    await bot.send_poll(
+        chat_id=chat_id,
         question=question,
         options=options,
         is_anonymous=False,
@@ -25,12 +22,12 @@ async def send_movie_poll(*, config: Config, bot: telegram.Bot):
     )
 
 
-async def send_participation_poll(*, config: Config, bot: telegram.Bot):
+async def send_participation_poll(*, bot: telegram.Bot, chat_id: int) -> None:
     question = "Ich bin"
     options = ["dabei", "nicht dabei", "vielleicht dabei"]
 
-    return await bot.send_poll(
-        chat_id=config.telegram.poll_chat_id,
+    await bot.send_poll(
+        chat_id=chat_id,
         question=question,
         options=options,
         is_anonymous=False,
